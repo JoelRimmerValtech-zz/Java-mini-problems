c29529a6fc0d854f484ad29a9aed98598ffad76e
Joel Rimmer <joel.rimmer@valtech.com>
2019-03-01 10:19:00 +0000

    Greetings program complete

---
+++ src/joel/mini/programs/Greetings.java
@@ -0,0 +1,22 @@
+package joel.mini.programs;
+
+import java.util.Scanner;
+
+public class Greetings {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, what is your name?");
+        System.out.print("My name is: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+
+        if (name.equals("Alice") || name.equals("Bob")) {
+            System.out.print("Hello " + name + ", welcome to my program!");
+        } else {
+            System.out.print("Sorry, I do not recognise you.");
+        }
+
+
+    }
+}
